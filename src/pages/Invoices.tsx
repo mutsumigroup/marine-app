74bdadd5055d73fb94eeadf4b4e9d906e1e1935a
中村謙太郎 <nakamurakentarou@nakamurakentarounoMacBook-Air.local>
2026-07-30 22:33:19 +0900

    fix: keyをpreviewInv.idのみに変更

--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -341,7 +341,7 @@ export default function Invoices({ invoices, reports, settings, onSend, onPaid, 
           })
         }
       </div>
-      {previewInv && <InvoiceSheet key={previewInv.id + String(previewInv.subtotal) + String(previewInv.expenses)} inv={previewInv} reports={reports} settings={settings} onClose={() => setPreviewId(null)} onSend={handleSend} onUpdateInvoice={onUpdateInvoice} processing={processing===previewInv.id} />}
+      {previewInv && <InvoiceSheet key={previewInv.id} inv={previewInv} reports={reports} settings={settings} onClose={() => setPreviewId(null)} onSend={handleSend} onUpdateInvoice={onUpdateInvoice} processing={processing===previewInv.id} />}
     </div>
   )
 }
